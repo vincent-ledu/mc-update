72fc7afecf7830d419591de8478b9a3a4c0aa26d
Vincent LE DU <vincent.ledu@ca-cib.com>
2020-03-03 17:01:34 +0100

    set right chmod to udpate.sh

--- update.sh
+++ update.sh
@@ -7,27 +7,31 @@ MC_VERSION_LINK=$(node index.js release)
 MC_VERSION=$(echo $MC_VERSION_LINK | cut -d" " -f1)
 MC_LINK=$(echo $MC_VERSION_LINK | cut -d" " -f2)
 
-wget $MC_LINK -O /tmp/mc/server-$MC_VERSION.jar
+wget --quiet $MC_LINK -O /tmp/mc/server-$MC_VERSION.jar
 
 CK_NEWSERVER=$(cksum /tmp/mc/server-$MC_VERSION.jar | cut -d' ' -f1,2)
 CK_OLDSERVER=$(cksum $MC_HOME/server.jar | cut -d' ' -f1,2)
+echo "CK_NEWSERVER: $CK_NEWSERVER"
+echo "CK_OLDSERVER: $CK_OLDSERVER"
 
 if [ "$CK_NEWSERVER" = "$CK_OLDSERVER" ]; then
-    echo " nothing to do, already latest release"
+    echo "************* Nothing to do, already latest release"
 else
-    echo " updating server"
+    echo "************* updating server"
     # kill actual server
     screen -X stuff 'stop^M'
     sleep 10
     # backup games
     GAMES_FOLDERS=$(cd $MC_HOME && ls -d */ | grep -v logs | grep -v crash-reports | grep -v latest-game)
-    mkdir -p $MC_BACKUP_GAMES_FOLDER
+    echo "GAMES_FOLDERS: $GAMES_FOLDERS"
+    mkdir -pv $MC_BACKUP_GAMES_FOLDER
+
     for folder in $GAMES_FOLDER; do
         cp -vr folder $MC_BACKUP_GAMES_FOLDER
     done
     # update server
     unlink $MC_HOME/server.jar
-    cp /tmp/mc/server-$MC_VERSION_LINK.jar $MC_HOME/
+    cp -v /tmp/mc/server-$MC_VERSION_LINK.jar $MC_HOME/
     ln -s $MC_HOME/server-$MC_VERSION_LINK.jar $MC_HOME/server.jar
     # relaunch server
     screen -X stuff './launch_minecraft_server.sh latest-game'
