7e0454d99e9b23fbdd6f466318b76913e8752b12
Vincent LE DU <vincent.ledu@ca-cib.com>
2020-03-03 18:25:48 +0100

    set right chmod to udpate.sh

---
+++ backup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+MC_HOME=/opt/minecraft/server
+
+screen -X stuff 'save-off^M'
+screen -X stuff 'save-all^M'
+
+sleep 10
+
+CUR_VERSION=$(cat $MC_HOME/version.txt)
+MC_BACKUP_GAMES_FOLDER=/opt/minecraft/backup_games/$(date +%Y-%m-%d_%H%M%S)-$CUR_VERSION
+GAMES_FOLDERS=$(cd $MC_HOME && ls -d */ | grep -v logs | grep -v crash-reports | grep -v latest-game)
+echo "GAMES_FOLDERS: $GAMES_FOLDERS"
+mkdir -pv $MC_BACKUP_GAMES_FOLDER
+
+for folder in $GAMES_FOLDERS; do
+    cp -r $MC_HOME/$folder $MC_BACKUP_GAMES_FOLDER
+done
+
+
+screen -X stuff 'save-on^M'
